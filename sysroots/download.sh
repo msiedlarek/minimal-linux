7db1d9bd4d0f3302a89ab7e8f09d2a044a584db5
Mikołaj Siedlarek <mikolaj@siedlarek.net>
2023-05-06 12:45:25 +0200

    Add Alpine-based sysroots for cross-compilation.

---
+++ sysroots/download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+SYSROOTS_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+for arch in x86_64 aarch64; do
+    sysroot_dir="${SYSROOTS_DIR}/${arch}"
+    if [ ! -f "${sysroot_dir}/lib/libc.a" ]; then
+        mkdir -p "${sysroot_dir}"
+        curl --location --fail "https://dl-cdn.alpinelinux.org/alpine/v3.18/main/${arch}/linux-headers-6.3-r0.apk" \
+            | tar -C "${sysroot_dir}" --strip-components=1 -xzf -
+        curl --location --fail "https://dl-cdn.alpinelinux.org/alpine/v3.18/main/${arch}/musl-dev-1.2.4-r0.apk" \
+            | tar -C "${sysroot_dir}" --strip-components=1 -xzf -
+    fi
+done
---
+++ sysroots/x86_64/include/linux/kernel.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _LINUX_KERNEL_H
+#define _LINUX_KERNEL_H
+
+#ifdef __GLIBC__
+#include <linux/sysinfo.h>
+#endif
+#include <linux/const.h>
+
+#endif /* _LINUX_KERNEL_H */
